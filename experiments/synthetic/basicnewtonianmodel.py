3b07d20fdc8c7c745438ba7ee5a6ef145119c9bf
William Diedrichsen Marstran <wdmdev@gmail.com>
2021-09-06 17:40:17 +0200

    Initial commit
    Setting up a structure

---
+++ experiments/synthetic/basicnewtonianmodel.py
@@ -0,0 +1,14 @@
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__)))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+
+from data.synthetic import nhpp
+from models import basicnewtonian
+
+if __name__ == '__main__':
+    # Synthesize data from non-homogeneous Poisson process
+
+    # Model simulation of intensity function
+
+    # Sample node pair events
+    raise NotImplementedError('Not Implemented')
