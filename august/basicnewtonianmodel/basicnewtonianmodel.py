4f793d1723004d7c76de28176ede119192f0d07f
augustsemrau <augu0093@gmail.com>
2021-09-09 13:53:00 +0200

    Personal folder

---
+++ august/basicnewtonianmodel/basicnewtonianmodel.py
@@ -0,0 +1,29 @@
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__)))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+from datasets.synthetic.basicnewtonianmodel import *
+
+# A simple example for 2 nodes
+seed = 1000
+
+# Set the initial position and velocity
+x0 = np.asarray([[-3, 0], [3, 0]])
+v0 = np.asarray([[1, 0], [-1, 0]])
+
+# Get the number of nodes and dimension size
+numOfNodes = x0.shape[0]
+dim = x0.shape[1]
+
+# Set the max time
+maxTime = 60000
+
+# Bias values for nodes
+gamma = 0.5 * np.ones(shape=(numOfNodes, ))
+
+bnm = BasicNewtonianModel(x0=x0, v0=v0, maxTime=maxTime, gamma=gamma, seed=seed)
+events = bnm.sampleEventsForAllNodePairs()
+
+for i in range(numOfNodes):
+    for j in range(i+1, numOfNodes):
+        print("Events for node pair ({}-{}): {}".format(i, j, events[i][j]))
