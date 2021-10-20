e44b8c7a2eb582be9c1c05333e0cbb036de5625e
augustsemrau <augu0093@gmail.com>
2021-10-20 13:22:34 +0200

    fixedanalytical integral

--- src/utils/integrals/analytical.py
+++ src/utils/integrals/analytical.py
@@ -1,8 +1,9 @@
 import torch
 import numpy as np
 
-def analytical_integral(t0:torch.Tensor, tn:torch.Tensor, z:torch.Tensor, 
-                        v:torch.Tensor, i:int, j:int, beta:torch.Tensor) -> torch.Tensor:
+def analytical_integral(t0:torch.Tensor, tn:torch.Tensor, 
+                        i:int, j:int,
+                        z:torch.Tensor, v:torch.Tensor,  beta:torch.Tensor) -> torch.Tensor:
     '''
     Calculates the Riemann sum for the integral from t0 to tn
     based on the nodes i and j and the given function func.
@@ -17,32 +18,44 @@ def analytical_integral(t0:torch.Tensor, tn:torch.Tensor, z:torch.Tensor,
 
     :returns:           The closed form solution of the squared euclidean intensity function
     '''
-    pos_i, pos_j = z[i, :], z[j, :]
-    xi, yi, xj, yj = pos_i[0], pos_i[1], pos_j[0], pos_j[1]
-    vi, vj = v[i, :], v[j, :]
-    vxi, vyi, vxj, vyj = vi[0], vi[1], vj[0], vj[1]
 
-    a = xi-xj
-    m = vxi - vxj
+    xi, xj, yi, yj      = z[i,0], z[j,0], z[i,1], z[j,1]
+    vxi, vxj, vyi, vyj  = v[i,0], v[j,0], v[i,1], v[j,1]
+
+    a = xi - xj
     b = yi - yj
+    m = vxi - vxj
     n = vyi - vyj
 
-    sqb = torch.square(b)
-    sqm = torch.square(m)
-    sqn = torch.square(n)
-    sqrtmn = torch.sqrt(sqm + sqn)
-    psqmn = sqm+sqn
-
-    return  (((torch.sqrt(torch.pi))*torch.exp(((-sqb-a+beta)*sqm-sqn*(-beta+a))/(psqmn)))
-                /(2*sqrtmn) 
-                *
+    ## Simons integral
+    return (    -   
                 (
-                    torch.erf(((psqmn)*tn + b*n)/(sqrtmn)) -
-                    torch.erf(((psqmn)*t0+b*n)/(sqrtmn))
+                torch.sqrt(torch.pi) / (2*torch.sqrt(m**2 + n**2))
                 )
-             )
-    # Simons
-    # return -torch.sqrt(torch.pi)*torch.exp(((-b**2 + beta)*m**2 + 2*a*b*m*n - n**2*(a**2 - beta))/(m**2 + n**2))*(torch.erf(((m**2 + n**2)*t0 + a*m + b*n)/torch.sqrt(m**2 + n**2)) - torch.erf(((m**2 + n**2)*tn + a*m + b*n)/torch.sqrt(m**2 + n**2)))/(2*torch.sqrt(m**2 + n**2))
+                *
+                ( 
+                torch.exp(((-b**2 + beta) * m**2 + 2*a*b*m*n - n**2 * (a**2 - beta)) / (m**2 + n**2))
+                ) 
+                * 
+                (torch.erf(((m**2 + n**2)*t0 + a*m + b*n) / torch.sqrt(m**2 + n**2)) - 
+                torch.erf(((m**2 + n**2)*tn + a*m + b*n) / torch.sqrt(m**2 + n**2))) 
+            )
+
+    # ## Our integral
+    # return (    -   
+    #             (
+    #             torch.sqrt(torch.pi) / (2*torch.sqrt(m**2 + n**2))
+    #             )
+    #             *
+    #             ( 
+    #             torch.exp(((-b**2 - a + beta) * m**2 - n**2 * (a - beta)) / (m**2 + n**2))
+    #             ) 
+    #             * 
+    #             (torch.erf(((m**2 + n**2)*t0 + b*n) / torch.sqrt(m**2 + n**2)) - 
+    #             torch.erf(((m**2 + n**2)*tn + b*n) / torch.sqrt(m**2 + n**2))) 
+    #         )
+
+
 
 def vec_analytical_integral(t0:torch.Tensor, tn:torch.Tensor, 
                             z0:torch.Tensor, v0:torch.Tensor, beta:torch.Tensor, device):
