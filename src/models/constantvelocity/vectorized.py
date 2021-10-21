Fix calculation of vectorized event intensity
Problem was that all node pair intensities were summed. Fix was to only sum intensities of the node pairs which had recorded interactions in the given data

src/models/constantvelocity/vectorized.py
@@ -1,7 +1,9 @@
 import torch
 import torch.nn as nn
+from models.constantvelocity.standard_simon import SimonConstantVelocityModel
 from utils.nodes.distances import vec_squared_euclidean_dist
 from utils.integrals.analytical import vec_analytical_integral as evaluate_integral
+from utils.integrals.analytical import analytical_integral as sim_evaluate_integral
 
 
 class VectorizedConstantVelocityModel(nn.Module):
@@ -26,6 +28,7 @@ class VectorizedConstantVelocityModel(nn.Module):
             self.num_of_nodes = n_points
             self.n_node_pairs = n_points*(n_points-1) // 2
             self.node_pair_idxs = torch.triu_indices(row=self.num_of_nodes, col=self.num_of_nodes, offset=1)
+            self.sim_model = SimonConstantVelocityModel(n_points, beta)
 
 
     def steps(self, times:torch.Tensor) -> torch.Tensor:
@@ -57,7 +60,7 @@ class VectorizedConstantVelocityModel(nn.Module):
         z = self.steps(times)
         d = vec_squared_euclidean_dist(z)
         #Only take upper triangular part, since the distance matrix is symmetric and exclude node distance to same node
-        return torch.triu(self.beta - d.triu(), diagonal=1)
+        return (self.beta - d).triu(diagonal=1)
 
 
     def forward(self, data:torch.Tensor, t0:torch.Tensor, tn:torch.Tensor) -> torch.Tensor:
@@ -70,11 +73,13 @@ class VectorizedConstantVelocityModel(nn.Module):
 
         :returns:       Log liklihood of the model based on the given data
         '''
-        # Z = self.steps(data[:,2])
-        event_intensity = torch.sum(self.log_intensity_function(times=data[:,2]))
+        log_intensities = self.log_intensity_function(times=data[:,2])
+        t = list(range(data.size()[0]))
+        i = [int(d) for d in data[:,0].tolist()]
+        j = [int(d) for d in data[:,1].tolist()]
+        event_intensity = torch.sum(log_intensities[t, i, j])
         non_event_intensity = torch.sum(evaluate_integral(t0, tn, 
                                                         z0=self.z0, v0=self.v0, 
                                                         beta=self.beta, device=self.device).triu(diagonal=1))
-
         # Logliklihood
         return event_intensity - non_event_intensity