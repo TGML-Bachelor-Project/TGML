25c70a9a3f758403ceed91b932bf92a56314cc12
augustsemrau <augu0093@gmail.com>
2021-11-03 10:34:42 +0100

    results by removing interactions

---
+++ src/utils/results_evaluation/remove_interactions.py
@@ -0,0 +1,13 @@
+import numpy as np
+import torch
+from sklearn.model_selection import train_test_split
+
+
+def remove_interactions(dataset, percentage, device):
+
+    dataset_reduced = train_test_split(dataset, test_size=percentage, random_state=1)
+
+
+    return dataset_reduced
+
+
