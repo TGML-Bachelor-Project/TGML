f915335f6f58168edde377538e4e78f8ebfb91e6
augustsemrau <augu0093@gmail.com>
2021-11-04 14:51:05 +0100

    results

--- src/utils/results_evaluation/remove_interactions.py
+++ src/utils/results_evaluation/remove_interactions.py
@@ -2,7 +2,8 @@ import random
 random.seed(1)
 import numpy as np
 import torch
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_curve, roc_auc_score
+import matplotlib.pyplot as plt
 
 
 def remove_interactions(dataset, percentage, device):
@@ -36,5 +37,42 @@ def auc_removed_interactions(removed_interactions, num_nodes, result_model):
         tup_copy[0], tup_copy[1] = alternate_node_pair[0], alternate_node_pair[1]
         alternate_node_pairs.append(tup_copy)
 
-    auc_score = 1
-    return auc_score
+    ## Then we create a new dataset with labels which can be evaluated
+    test_set = []
+    labels = []
+    for i in range(len(removed_interactions.tolist())):
+        if random.randint(0,1) == 0:
+            test_set.append(alternate_node_pairs[i])
+            labels.append(0)
+        else:
+            test_set.append(removed_interactions[i])
+            labels.append(1)
+
+    ## Compute probability for node pair interaction for test set
+    probs = []
+    for tup in test_set:
+        probs.append(result_model.log_intensity_function(i=tup[0], j=tup[1], t=tup[2]))
+
+    ## Compute ROC metrics
+    fpr, tpr, thresh = roc_curve(labels, probs, pos_label=1)
+    auc_score = roc_auc_score(labels, probs)
+
+    ## Plot ROC Curve
+    plt.style.use('seaborn')
+    # plot roc curves
+    plt.plot(fpr, tpr, linestyle='--',color='orange', label='Constant Velocity Model')
+    random_probs = [0 for i in range(len(labels))]
+    p_fpr, p_tpr, _ = roc_curve(labels, random_probs, pos_label=1)
+    plt.plot(p_fpr, p_tpr, linestyle='--', color='blue')
+    # title
+    plt.title('ROC curve')
+    # x label
+    plt.xlabel('False Positive Rate')
+    # y label
+    plt.ylabel('True Positive rate')
+
+    plt.legend(loc='best')
+    plt.savefig('ROC',dpi=300)
+    plt.show()
+
+    return fpr, tpr, thresh, auc_score
