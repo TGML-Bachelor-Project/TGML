3f51958dcdad5480939a0b80e51790bccabdd091
William Diedrichsen Marstrand <wdmdev@gmail.com>
2021-11-05 15:40:45 +0100

    compare_intensities single plot fix

--- src/utils/report_plots/compare_intensity_rates.py
+++ src/utils/report_plots/compare_intensity_rates.py
@@ -24,17 +24,25 @@ def compare_intensity_rates_plot(train_t, result_model, gt_model, nodes, wandb_h
 
 
     ## Plot
-    fig, axs = plt.subplots(1,len(res_list), figsize=(5*len(nodes), 6), facecolor='w', edgecolor='k')
-    fig.subplots_adjust(hspace = .5, wspace=.5)
+    if len(res_list) == 1:
+        fig, ax = plt.subplots(1,len(res_list), figsize=(5*len(nodes), 6), facecolor='w', edgecolor='k')
+        ax.grid()
+        ax.plot(train_t, res_list[i], color="red", label="est.")
+        ax.plot(train_t, gt_list[i] , color="blue", label="gt")
+        ax.legend()
+        ax.set_title(f"Interactions Intensity Node {nodes[i][0]} and {nodes[i][1]}")
+    else:
+        fig, axs = plt.subplots(1,len(res_list), figsize=(5*len(nodes), 6), facecolor='w', edgecolor='k')
+        fig.subplots_adjust(hspace = .5, wspace=.5)
 
-    axs = axs.ravel()
+        axs = axs.ravel()
 
-    for i in range(len(res_list)):
-        axs[i].grid()
-        axs[i].plot(train_t, res_list[i], color="red", label="est.")
-        axs[i].plot(train_t, gt_list[i] , color="blue", label="gt")
-        axs[i].legend()
-        axs[i].set_title(f"Interactions Intensity Node {nodes[i][0]} and {nodes[i][1]}")
+        for i in range(len(res_list)):
+            axs[i].grid()
+            axs[i].plot(train_t, res_list[i], color="red", label="est.")
+            axs[i].plot(train_t, gt_list[i] , color="blue", label="gt")
+            axs[i].legend()
+            axs[i].set_title(f"Interactions Intensity Node {nodes[i][0]} and {nodes[i][1]}")
 
 
     wandb_handler.log({'intensity_plot': wandb_handler.Image(fig)})
