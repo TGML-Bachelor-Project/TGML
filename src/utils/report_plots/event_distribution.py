d76a62fd2658531197881896276856ef38fb5f2f
augustsemrau <augu0093@gmail.com>
2021-11-06 16:48:22 +0100

    logging dist plot with wandb

--- src/utils/report_plots/event_distribution.py
+++ src/utils/report_plots/event_distribution.py
@@ -2,10 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_event_dist(dataset):
+def plot_event_dist(dataset, wandb_handler):
     event_times = dataset[:,2].tolist()
-    plt.hist(event_times, bins = 100)
+    fig, ax = plt.subplots(1,1, figsize=(10, 6), facecolor='w', edgecolor='k')
+    ax.hist(event_times, bins = 100)
     # plt.show()
+    wandb_handler.log({'distribution_plot': wandb_handler.Image(fig)})
     return
 
 
