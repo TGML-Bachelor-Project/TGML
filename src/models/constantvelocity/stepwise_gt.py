15952b69fa1b84715577596d3b1a8985502b4a4e
William Diedrichsen Marstrand <wdmdev@gmail.com>
2021-11-08 21:11:40 +0100

    detach torch error

--- src/models/constantvelocity/stepwise_gt.py
+++ src/models/constantvelocity/stepwise_gt.py
@@ -21,10 +21,10 @@ class GTStepwiseConstantVelocityModel(nn.Module):
     
             self.device = device
             self.num_of_steps = steps
-            self.beta = nn.Parameter(torch.tensor([[beta]]), requires_grad=False).to(self.device)
+            self.beta = nn.Parameter(beta.clone().detach(), requires_grad=False).to(self.device)
             z0_copy = z.astype(np.float) if isinstance(z, np.ndarray) else z
-            v0_copy = v.detach().clone()
-            self.z0 = nn.Parameter(torch.tensor(z0_copy), requires_grad=False).to(self.device) 
+            v0_copy = v.clone().detach()
+            self.z0 = nn.Parameter(torch.from_numpy(z0_copy), requires_grad=False).to(self.device) 
             self.v0 = nn.Parameter(v0_copy, requires_grad=False).to(self.device)
     
             self.num_of_nodes = n_points
