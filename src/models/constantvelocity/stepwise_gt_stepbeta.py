a3719942d980960ad919691960a29c5e243f1acf
William Diedrichsen Marstrand <wdmdev@gmail.com>
2021-11-08 21:05:15 +0100

    step beta GT model device error fix

--- src/models/constantvelocity/stepwise_gt_stepbeta.py
+++ src/models/constantvelocity/stepwise_gt_stepbeta.py
@@ -21,11 +21,11 @@ class GTStepwiseConstantVelocityModel(nn.Module):
     
             self.device = device
             self.num_of_steps = steps
-            self.beta = nn.Parameter(beta, requires_grad=False)
+            self.beta = nn.Parameter(beta, requires_grad=False).to(device)
             z0_copy = z.astype(np.float) if isinstance(z, np.ndarray) else z
-            v0_copy = v.detach().clone().to(device)
-            self.z0 = nn.Parameter(torch.tensor(z0_copy).to(device), requires_grad=False) 
-            self.v0 = nn.Parameter(v0_copy, requires_grad=False) 
+            v0_copy = v.detach().clone()
+            self.z0 = nn.Parameter(torch.tensor(z0_copy), requires_grad=False).to(device)
+            self.v0 = nn.Parameter(v0_copy, requires_grad=False).to(device)
     
             self.num_of_nodes = n_points
             self.node_pair_idxs = torch.triu_indices(row=self.num_of_nodes, col=self.num_of_nodes, offset=1)
