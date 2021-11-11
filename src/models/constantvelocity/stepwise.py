74ed9f957c697012a2f85b27bbd4f325b20e3cb2
William Diedrichsen Marstrand <wdmdev@gmail.com>
2021-11-11 12:44:10 +0100

    working on making steps scalable

--- src/models/constantvelocity/stepwise.py
+++ src/models/constantvelocity/stepwise.py
@@ -38,10 +38,10 @@ class StepwiseVectorizedConstantVelocityModel(nn.Module):
             # Creating the time step deltas
             #Equally distributed
             time_intervals = torch.linspace(0, max_time, steps+1)
-            start_times = time_intervals[:-1]
-            end_times = time_intervals[1:]
-            self.time_intervals = list(zip(start_times.tolist(), end_times.tolist()))
-            self.time_deltas = (end_times-start_times).to(self.device)
+            self.start_times = time_intervals[:-1]
+            self.end_times = time_intervals[1:]
+            self.time_intervals = list(zip(self.start_times.tolist(), self.end_times.tolist()))
+            self.time_deltas = (self.end_times-self.start_times).to(self.device)
             # All deltas should be equal do to linspace, so we can take the first
             self.step_size = self.time_deltas[0].to(self.device)
 
@@ -79,13 +79,12 @@ class StepwiseVectorizedConstantVelocityModel(nn.Module):
         time_step_indices = [ t if t < self.num_of_steps else t-1 for t in  time_to_step_index.tolist()]
         #Calculate the remainding time that will be inside the matching step for each time
         remainding_time = (times-torch.tensor(time_step_indices).to(self.device)*self.step_size)
-        #The step positions we will start from for each time point and then use to find their actual position
-        Z_step_starting_positions = steps_z0[:,:,time_step_indices]
+
         #Latent Z positions for all times
-        Zt = Z_step_starting_positions + self.v0[:,:,time_step_indices]*remainding_time
+        zt = steps_z0[:,:,time_step_indices] + self.v0[:,:,time_step_indices]*remainding_time
 
         # We don't take the very last z0, because that is the final z positions and not the start of any new step
-        return Zt, steps_z0[:,:,:-1]
+        return zt, steps_z0[:,:,:-1]
 
     def old_log_intensity_function(self, v0, times:torch.Tensor):
         '''
