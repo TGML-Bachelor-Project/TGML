46604db30ead448dca18e4ae16b5e3981601b4cd
William Diedrichsen Marstrand <wdmdev@gmail.com>
2021-11-12 18:01:18 +0100

    Trying new sq euclidean dist calculation to decrease memory usage

--- src/utils/nodes/distances.py
+++ src/utils/nodes/distances.py
@@ -23,4 +23,9 @@ def old_vec_squared_euclidean_dist(Z):
     return torch.cdist(Z,Z,2)**2
 
 def vec_squared_euclidean_dist(Z):
-    return torch.sum(torch.square(Z.unsqueeze(0)-Z.unsqueeze(1)), dim=2)
+    # We permute Z to [Steps x Nodes x Dimensions] to use cdist
+    steps_first_Z = torch.permute(Z, (2,0,1))
+    #dist1 = torch.sum(torch.square(Z.unsqueeze(0)-Z.unsqueeze(1)), dim=2)
+    dist = torch.permute(torch.cdist(steps_first_Z,steps_first_Z)**2, (1,2,0))
+
+    return dist
