ab4f3026bc3a94ce6d868f7959eaeed10423dd27
William Diedrichsen Marstrand <wdmdev@gmail.com>
2021-11-15 07:55:00 +0100

    Working on batching

--- src/models/constantvelocity/stepwise.py
+++ src/models/constantvelocity/stepwise.py
@@ -24,7 +24,7 @@ class StepwiseVectorizedConstantVelocityModel(nn.Module):
     
             self.gamma = gamma
             self.time_batch_size = time_batch_size
-            self.node_batch_size = node_batch_size
+            self.node_pair_batch_size = node_batch_size
             self.device = device
             self.num_of_steps = steps
             self.beta = nn.Parameter(torch.tensor([[beta]]), requires_grad=True)
@@ -78,7 +78,7 @@ class StepwiseVectorizedConstantVelocityModel(nn.Module):
         # We don't take the very last z0, because that is the final z positions and not the start of any new step
         return steps_z0[:,:,:-1]
 
-    def steps(self, times:torch.Tensor) -> torch.Tensor:
+    def steps(self, nodes, times:torch.Tensor) -> torch.Tensor:
         '''
         Increments the model's time by t by
         updating the latent node position vector z
@@ -88,18 +88,15 @@ class StepwiseVectorizedConstantVelocityModel(nn.Module):
 
         :returns:   The updated latent position vector z
         '''
-        steps_z0 = self.z0.unsqueeze(2) + torch.cumsum(self.v0*self.time_deltas, dim=2)
-        # Adding the initial Z0 position as first step and removes last step, as it is not a starting position
-        steps_z0 = torch.cat((self.z0.unsqueeze(2), steps_z0), dim=2)[:,:,:-1]
         step_mask = ((times.unsqueeze(1) > self.start_times) | (self.start_times == 0).unsqueeze(0))
         step_end_times = step_mask*torch.cumsum(step_mask*self.step_size, axis=1)
         time_mask = times.unsqueeze(1) <= step_end_times
         time_deltas = (self.step_size - (step_end_times - times.unsqueeze(1))*time_mask)*step_mask
         
-        movement = torch.sum(self.v0.unsqueeze(2)*time_deltas, dim=3)
+        movement = torch.sum(self.v0[nodes].unsqueeze(2)*time_deltas, dim=3)
 
         #Latent Z positions for all times
-        zt = self.z0.unsqueeze(2) + movement
+        zt = self.z0[nodes].unsqueeze(2) + movement
         return zt
 
     def old_log_intensity_function(self, times:torch.Tensor):
@@ -119,7 +116,7 @@ class StepwiseVectorizedConstantVelocityModel(nn.Module):
         #Only take upper triangular part, since the distance matrix is symmetric and exclude node distance to same node
         return self.beta - d
 
-    def log_intensity_function(self, times:torch.Tensor):
+    def log_intensity_function(self, nodes, times:torch.Tensor):
         '''
         The log version of the  model intensity function between node i and j at time t.
         The intensity function measures the likelihood of node i and j
@@ -131,7 +128,7 @@ class StepwiseVectorizedConstantVelocityModel(nn.Module):
         :returns:   The log of the intensity between i and j at time t as a measure of
                     the two nodes' log-likelihood of interacting.
         '''
-        Zt = self.steps(times)
+        Zt = self.steps(nodes, times)
         d = vec_squared_euclidean_dist(Zt)
         #Only take upper triangular part, since the distance matrix is symmetric and exclude node distance to same node
         return self.beta - d
@@ -144,6 +141,69 @@ class StepwiseVectorizedConstantVelocityModel(nn.Module):
         sq_frob_norms = torch.square(torch.linalg.norm(velocity_changes))
         return  log_likelihood + self.gamma * torch.sum(sq_frob_norms)
 
+    def forward_copy(self, data:torch.Tensor, t0:torch.Tensor, tn:torch.Tensor) -> torch.Tensor:
+        '''
+        Standard torch method for training of the model.
+
+        :param data:    Node pair interaction data with columns [node_i, node_j, time_point]
+        :param t0:      Start of the interaction period
+        :param tn:      End of the interaction period
+
+        :returns:       Log liklihood of the model based on the given data
+        '''
+        event_intensity = torch.tensor(0.)
+        unique_times, unique_time_counts = torch.unique(data[:,2], return_counts=True)
+        unique_time_batches = (torch.split(unique_times, self.time_batch_size) 
+                                if self.time_batch_size > 0 else unique_times)
+        unique_time_count_batches = (torch.split(unique_time_counts, self.time_batch_size) 
+                                        if self.time_batch_size > 0 else unique_time_counts)
+
+        # Batching by time
+        current_time_index = 0
+        for batch_id in range(len(unique_time_batches)):
+            time_batch = unique_time_batches[batch_id]
+            counts = unique_time_count_batches[batch_id]
+            # Use counts of unique time points to calculate current data batch end index
+            batch_end_time_index = current_time_index + torch.sum(counts)
+            # Compute the indices of each unique time point in the batch
+            _, batch_time_indices = torch.unique(data[current_time_index:batch_end_time_index,2], return_inverse=True)
+
+            # Batching by nodes
+            # Sorting node pairs to avoid duplicates in unique e.g. both [1,3] and [3,1]. We only want one of them
+            batch_node_pairs, _ = torch.sort(data[current_time_index:batch_end_time_index, :2].long(), dim=0)
+            unique_node_pairs, unique_node_indices = torch.unique(batch_node_pairs, dim=0, return_inverse=True)
+            nodes = torch.unique(unique_node_pairs)
+            node_batch_size = self.node_pair_batch_size if self.node_pair_batch_size > 0 else unique_node_pairs.shape[0]
+            node_batches = torch.split(nodes, node_batch_size)
+
+            for node_batch in node_batches:
+                nodes, node_indices = torch.unique(node_batch, return_inverse=True)
+                node_time_indices = unique_node_indices[unique_node_indices in node_indices]
+                
+                log_intensities = self.log_intensity_function(nodes, times=time_batch)
+                event_intensity += torch.sum(log_intensities[node_batch[unique_node_indices,0], 
+                                            node_batch[unique_node_indices,1], batch_time_indices]) 
+
+            current_time_index += batch_end_time_index
+
+        old_log_intensities = self.old_log_intensity_function(times=data[:,2])
+        t = list(range(data.shape[0]))
+        i = torch.floor(data[:,0]).tolist() #torch.floor to make i and j int
+        j = torch.floor(data[:,1]).tolist()
+        old_event_intensities = torch.sum(old_log_intensities[i,j,t])
+
+        all_integrals = evaluate_integral(t0, tn, 
+                                    z0=self.steps_z0(), v0=self.v0, 
+                                    beta=self.beta, device=self.device)
+        #Sum over time dimension, dim 2, and then sum upper triangular
+        integral = torch.sum(torch.sum(all_integrals,dim=2).triu(diagonal=1))
+        non_event_intensity = torch.sum(integral)
+
+        log_likelihood =  event_intensity - non_event_intensity 
+
+        # Regularize model on velocity change if gamma is set
+        return self.regularize(log_likelihood) if self.gamma else log_likelihood
+
     def forward(self, data:torch.Tensor, t0:torch.Tensor, tn:torch.Tensor) -> torch.Tensor:
         '''
         Standard torch method for training of the model.
@@ -154,19 +214,34 @@ class StepwiseVectorizedConstantVelocityModel(nn.Module):
 
         :returns:       Log liklihood of the model based on the given data
         '''
-        unique_times, unique_time_indices = torch.unique(data[:,2], return_inverse=True)
-        i = data[:,0].long() #long to make i and j int
-        j = data[:,1].long()
-        log_intensities = self.log_intensity_function(times=unique_times)
-        event_intensity = torch.sum(log_intensities[i,j,unique_time_indices]) 
+        event_intensity = torch.tensor(0.)
+        unique_node_pairs, _ = torch.sort(torch.unique(data[:,:2], dim=0), dim=1)
+        node__pair_splits = torch.split(unique_node_pairs, self.node_pair_batch_size)
 
-        '''
+        # Batching in node dimension
+        for node_pairs in node__pair_splits:
+            unique_nodes = torch.unique(node_pairs).long()
+            num_of_batch_nodes_in_nodes = torch.sum(data[:,:2] == node_pairs.unsqueeze(1), dim=2)
+            # == 2 because both node[0] and node[1] must be in the chosen nodes
+            batch_data = data[torch.sum(num_of_batch_nodes_in_nodes == 2, dim=0)] 
+            # batch_data = data[torch.isin(data[:,:2], node_pairs)]
+            unique_times = torch.unique(batch_data[:,2])
+            time_splits = torch.split(unique_times, self.time_batch_size)
+
+            # Batching in time dimension
+            for times in time_splits:
+                time_mask = torch.sum(batch_data[:,2] == times.unsqueeze(1), dim=0)
+                i = batch_data[time_mask][:,0].long()
+                j = batch_data[time_mask][:,1].long()
+                log_intensities = self.log_intensity_function(unique_nodes, times=times)
+                event_intensity += torch.sum(log_intensities[i,j])
+
+
         old_log_intensities = self.old_log_intensity_function(times=data[:,2])
         t = list(range(data.shape[0]))
         i = torch.floor(data[:,0]).tolist() #torch.floor to make i and j int
         j = torch.floor(data[:,1]).tolist()
         old_event_intensities = torch.sum(old_log_intensities[i,j,t])
-        '''
 
         all_integrals = evaluate_integral(t0, tn, 
                                     z0=self.steps_z0(), v0=self.v0, 
--- src/traintestgyms/ignitegym.py
+++ src/traintestgyms/ignitegym.py
@@ -60,7 +60,7 @@ class TrainTestGym:
 
     ### Training step
     def __train_step(self, engine, batch):
-        batch = batch.to(self.device, dtype=torch.float32)
+        batch = batch.to(self.device)
 
         if engine.t_start != 0:
             engine.t_start = batch[0,self.time_column_idx].item()
