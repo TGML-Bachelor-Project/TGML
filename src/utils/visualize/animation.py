950137a9331b5f7ed8c12cd80cb0619ec251a6d0
William Diedrichsen Marstrand <wdmdev@gmail.com>
2021-11-16 22:25:20 +0100

    Fixed auto play for animations

--- src/utils/visualize/animation.py
+++ src/utils/visualize/animation.py
@@ -24,22 +24,6 @@ def animate(model, t_start, t_end, num_of_time_points, device, wandb_handler):
     movement = torch.sum(v0.unsqueeze(2)*time_deltas, dim=3)
     step_zt = z0.unsqueeze(2) + movement
 
-    '''
-    #Calculate how many steps each time point corresponds to
-    time_step_ratio = times/step_size
-    #Make round down time_step_ratio to find the index of the step which each time fits into
-    time_step_indices = torch.floor(time_step_ratio)
-    #Make sure times that lands on tn is put into the last time step by subtracting 1 from their step index
-    time_step_indices = torch.tensor([t if t < num_of_steps else t-1 for t in time_step_indices])
-    #Calculate the remainding time that will be inside the matching step for each time
-    remainding_time = (times-time_step_indices*step_size)
-    time_step_indices = time_step_indices.tolist()
-    #The step positions we will start from for each time point and then use to find their actual position
-    z_step_starting_positions = steps_z0[:,:,time_step_indices]
-    #Latent Z positions for all times
-    step_zt = z_step_starting_positions + v0[:,:,time_step_indices]*remainding_time    
-    '''
-
     df = pd.DataFrame({
         'node': [str(n) for n in [*list(range(step_zt.shape[0]))]*len(times)],
         'x': step_zt[:,0,:].T.flatten().tolist(),
@@ -64,26 +48,19 @@ def animate(model, t_start, t_end, num_of_time_points, device, wandb_handler):
     wandb_handler.log({'animation': wandb_handler.Html(plotly.io.to_html(fig, auto_play=False))})
 
 
-def animate_nomodel(z0, v0, time_deltas, step_size, num_of_steps, t_start, t_end, num_of_time_points, device):
+def animate_nomodel(z0, v0, time_deltas, step_size, start_times, t_start, t_end, num_of_time_points, device):
     # Starting positions for each model step
     steps_z0 = z0.unsqueeze(2) + torch.cumsum(v0*time_deltas, dim=2)
     steps_z0 = torch.cat((z0.unsqueeze(2), steps_z0), dim=2)
         
     times = torch.linspace(t_start, t_end, num_of_time_points)
 
-    #Calculate how many steps each time point corresponds to
-    time_step_ratio = times/step_size
-    #Make round down time_step_ratio to find the index of the step which each time fits into
-    time_step_indices = torch.floor(time_step_ratio)
-    #Make sure times that lands on tn is put into the last time step by subtracting 1 from their step index
-    time_step_indices = torch.tensor([t if t < num_of_steps else t-1 for t in time_step_indices])
-    #Calculate the remainding time that will be inside the matching step for each time
-    remainding_time = (times-time_step_indices*step_size)
-    time_step_indices = time_step_indices.tolist()
-    #The step positions we will start from for each time point and then use to find their actual position
-    z_step_starting_positions = steps_z0[:,:,time_step_indices]
-    #Latent Z positions for all times
-    step_zt = z_step_starting_positions + v0[:,:,time_step_indices]*remainding_time    
+    step_mask = ((times.unsqueeze(1) > start_times) | (start_times == 0).unsqueeze(0))
+    step_end_times = step_mask*torch.cumsum(step_mask*step_size, axis=1)
+    time_mask = times.unsqueeze(1) <= step_end_times
+    time_deltas = (step_size - (step_end_times - times.unsqueeze(1))*time_mask)*step_mask
+    movement = torch.sum(v0.unsqueeze(2)*time_deltas, dim=3)
+    step_zt = z0.unsqueeze(2) + movement
 
     df = pd.DataFrame({
         'node': [str(n) for n in [*list(range(step_zt.shape[0]))]*len(times)],
@@ -107,4 +84,4 @@ def animate_nomodel(z0, v0, time_deltas, step_size, num_of_steps, t_start, t_end
                   selector=dict(mode='markers'))
 
     fig.show()
-    fig.write_html(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'animations', 'latest_animation.html'))
+    fig.write_html(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'animations', 'latest_animation.html'), auto_play=False)
