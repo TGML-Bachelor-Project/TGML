d2d5306f1521acbe6068b20871dd15206fd1251b
augustsemrau <augu0093@gmail.com>
2021-11-17 14:46:09 +0100

    fixed loading datasets

--- src/data/real/load_dataset.py
+++ src/data/real/load_dataset.py
@@ -5,7 +5,7 @@ from utils.report_plots.event_distribution import plot_event_dist_eu_data
 from utils.report_plots.event_distribution import plot_event_dist_resistance_data
 
 ### Function for loading the three datasets
-def load_real_dataset(dataset_path):
+def load_data(dataset_path):
     dataset = np.genfromtxt(dataset_path, delimiter=',')
     num_nodes = len(np.unique([dataset[:,0],dataset[:,1]]))
     return dataset, num_nodes
@@ -18,19 +18,19 @@ def load_real_dataset(dataset_number, debug):
     
     if dataset_number == 1:
         path = os.path.join(path,'datasets/resistance_game4.csv')
-        dataset, num_nodes = load_real_dataset(dataset_path=path)
+        dataset, num_nodes = load_data(dataset_path=path)
         print(f'Length of "Resistance Game 4" dataset: {len(dataset)}')
         model_beta = 4.
 
     elif dataset_number == 2:
         path = os.path.join(path, 'datasets/email_eu_core_temporal_500nodes.csv')
-        dataset, num_nodes = load_real_dataset(dataset_path=path)
+        dataset, num_nodes = load_data(dataset_path=path)
         print(f'Length of "EU Email" dataset: {len(dataset)}')
         model_beta = 10.
     
     elif dataset_number == 3:
         path = os.path.join(path, 'datasets/tij_pres_LyonSchool.csv')
-        dataset, num_nodes = load_real_dataset(dataset_path=path)
+        dataset, num_nodes = load_data(dataset_path=path)
         print(f'Length of "Lyon School" dataset: {len(dataset)}')
         model_beta = 1.
     
