bb1317c070a8d42883540573ab544d6ade0acea9
augustsemrau <augu0093@gmail.com>
2021-11-18 12:43:12 +0100

    remove drift now works with vec0

--- src/utils/nodes/remove_drift.py
+++ src/utils/nodes/remove_drift.py
@@ -5,13 +5,20 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 import torch
 from utils.visualize.animation import animate_nomodel
 
+def __try_numpy_to_tensor(t):
+    return t if isinstance(t, torch.Tensor) else torch.from_numpy(t)
+
 def center_z0(z0:torch.Tensor):
+    z0 = __try_numpy_to_tensor(z0)
     return z0 - torch.mean(z0, dim=0)
 
 def remove_v_drift(v0:torch.Tensor):
+    v0 = __try_numpy_to_tensor(v0)
     return v0 - torch.mean(v0, dim=0)
 
 def remove_rotation(z0:torch.Tensor, v0:torch.Tensor):
+    z0 = __try_numpy_to_tensor(z0)
+    v0 = __try_numpy_to_tensor(v0)
     zv = torch.vstack([z0, v0.reshape(-1,2)])
     U, S, VT = torch.linalg.svd(zv, full_matrices=False)
     new_coords = U * S.unsqueeze(0)
